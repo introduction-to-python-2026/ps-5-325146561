36e6b10c8da3de2b825ca8ba2cd0b4133db716a4
325146561 <noasela2002@gmail.com>
2025-11-20 14:05:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # Add the import statements for functions from string_utils.py and equation_utils.py here
-
+from string_utils import split_before_uppercases, split_at_digit
+from equation_utils import (
+    count_atoms_in_molecule,
+    parse_chemical_reaction,
+    count_atoms_in_reaction,
+    build_equations
+)
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
