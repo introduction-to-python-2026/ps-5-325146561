9c6962c5367992c579d75155ca1f62f55add4c37
325146561 <noasela2002@gmail.com>
2025-11-20 13:55:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
-    # 1.parse reaction
-    reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+    reactants, products = parse_chemical_reaction(reaction)
+    reactant_atoms = count_atoms_in_reaction(reactants)
     product_atoms = count_atoms_in_reaction(products)
 
-    # 2.build equation and solve
     equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    coefficients = my_solve(equations, coefficients) + [1]
+    raw = my_solve(equations, coefficients)
 
-    return coefficients # [1/3, 1, 2/3, 1]
+    # האחרון הוא ה־b האחרון (לפי build_equations החדש)
+    last = raw[-1]
+
+    normalized = [coef / last for coef in raw]
+
+    return normalized
 
